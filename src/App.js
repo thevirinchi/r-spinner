f4164289cd14d7e9e3646bae75bfe6756f768d59
z3t4z00k <zetazook@gmail.com>
2020-12-08 23:10:23 +0530

    Added spinner and it's state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Spinner from './components/Spinner'
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [loading, toggleLoading] = useState(false)
+
+    const handleLoading = () => {
+        toggleLoading(true)
+        setTimeout(function(){
+            toggleLoading(false)
+        }, 3000)
+    }
+
+    return (
+        <div className="App">
+            <Spinner loadingState={loading}/>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <a className="App-link" target="_blank" rel="noopener noreferrer" onClick={handleLoading}> Start Loading </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
